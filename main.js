46ce03e916d589a9bc0eb78831c7ae76d6ce8b9d
saúl z mtz <97975590+szmtz@users.noreply.github.com>
2023-10-24 20:31:36 +0000

    Update JS code

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ const first = document.body.firstElementChild;
 // the first element was the first child tag of the body -- the h1 tag
 first.innerHTML = 'BROWN BEARS ARE AWESOME!';
 
-// Access the parent element of the variable 'first'
+// use the .parentNode property to access the parent element of the variable 'first'
+
 const parentElement = first.parentNode;
 
 // Modify the backgroundColor of the parent element to 'beige'
