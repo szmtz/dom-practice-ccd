1dbeeaceabed44ae58c46abfb6231e9a649a4e3b
saúl z mtz <97975590+szmtz@users.noreply.github.com>
2023-10-24 20:30:29 +0000

    Add html boilerplate sample

---
+++ main.js
@@ -0,0 +1,13 @@
+// Get the first child of the document body
+//Create a variable called first and set it to the first child of the document body
+const first = document.body.firstElementChild;
+
+// Set the inner HTML of the first element to the following text
+// the first element was the first child tag of the body -- the h1 tag
+first.innerHTML = 'BROWN BEARS ARE AWESOME!';
+
+// Access the parent element of the variable 'first'
+const parentElement = first.parentNode;
+
+// Modify the backgroundColor of the parent element to 'beige'
+parentElement.style.backgroundColor = 'beige';
